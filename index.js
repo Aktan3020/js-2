eb1bb13214aed4c740aa91746974cebb1cd651c8
Aktan3020 <116088191+Aktan3020@users.noreply.github.com>
2022-12-05 13:06:55 +0300

    02

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
+
+console.log("Client to request to get data...", "loading started");
 let JsObject = {
-    brand:"HONDA",
-    model:"FIT",
-    age:2016,
-    owner:"Oleg"
+    brand: "HONDA",
+    model: "FIT",
+    age: 2016,
+    owner: "Oleg"
 }
- 
-let prom = new Promise( function (resolve){
-   let JSONdata = JSON.stringify(JsObject) ;
-    resolve(JSONdata);
-})
-prom.then(function(JSONd){
-    setTimeout(function uu(){
-        let newJSdd = JSON.parse(JSONd)
-      console.log(newJSdd)
-    },2000
-    )
-},function(){
-    console.log("ошибка")
-})
+setTimeout(function () {
+    let prom = new Promise(function (resolve) {
+        let JSONdata = JSON.stringify(JsObject);
+        resolve(JSONdata);
+
+    })
+    prom.then(function (JSONd) {
+        setTimeout(function () {
+            console.log("Server send jsObject--" + JsObject);
+           
+        }, 1000)
+        setTimeout(function uu() {
+            let newJSdd = JSON.parse(JSONd)
+ console.log("Preparing Data");
+            console.log(newJSdd)
+        }, 2000
+        )
+    }).catch(function () {
+        return console.log("ошибка")
+    })
+}, 2000)
